d44fca52e8be205244b0b3fbb9361cf58b1134a8
Abdullah Azam <abdullahazam640@gmail.com>
2025-03-04 23:49:16 +0800

    Code Pertemuan 1

---
+++ Peremuan1/Peremuan1/Form1.Designer.vb
@@ -0,0 +1,206 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnVariabel = New Button()
+        btnVariabel1 = New Button()
+        btnOperator = New Button()
+        btnOperator1 = New Button()
+        btnOperator2 = New Button()
+        btnIF = New Button()
+        btnIF1 = New Button()
+        btnIF2 = New Button()
+        btnIF3 = New Button()
+        btnCase = New Button()
+        btnForNext = New Button()
+        btnDoLoop = New Button()
+        btnDoLoop1 = New Button()
+        btnDoLoop2 = New Button()
+        SuspendLayout()
+        ' 
+        ' btnVariabel
+        ' 
+        btnVariabel.Location = New Point(12, 12)
+        btnVariabel.Name = "btnVariabel"
+        btnVariabel.Size = New Size(94, 29)
+        btnVariabel.TabIndex = 0
+        btnVariabel.Text = "Variabel"
+        btnVariabel.UseVisualStyleBackColor = True
+        ' 
+        ' btnVariabel1
+        ' 
+        btnVariabel1.Location = New Point(12, 47)
+        btnVariabel1.Name = "btnVariabel1"
+        btnVariabel1.Size = New Size(94, 29)
+        btnVariabel1.TabIndex = 1
+        btnVariabel1.Text = "Variabel1"
+        btnVariabel1.UseVisualStyleBackColor = True
+        ' 
+        ' btnOperator
+        ' 
+        btnOperator.Location = New Point(12, 118)
+        btnOperator.Name = "btnOperator"
+        btnOperator.Size = New Size(94, 29)
+        btnOperator.TabIndex = 2
+        btnOperator.Text = "Operator"
+        btnOperator.UseVisualStyleBackColor = True
+        ' 
+        ' btnOperator1
+        ' 
+        btnOperator1.Location = New Point(12, 153)
+        btnOperator1.Name = "btnOperator1"
+        btnOperator1.Size = New Size(94, 29)
+        btnOperator1.TabIndex = 3
+        btnOperator1.Text = "Operator1"
+        btnOperator1.UseVisualStyleBackColor = True
+        ' 
+        ' btnOperator2
+        ' 
+        btnOperator2.Location = New Point(12, 188)
+        btnOperator2.Name = "btnOperator2"
+        btnOperator2.Size = New Size(94, 29)
+        btnOperator2.TabIndex = 4
+        btnOperator2.Text = "Operator2"
+        btnOperator2.UseVisualStyleBackColor = True
+        ' 
+        ' btnIF
+        ' 
+        btnIF.Location = New Point(12, 246)
+        btnIF.Name = "btnIF"
+        btnIF.Size = New Size(94, 29)
+        btnIF.TabIndex = 5
+        btnIF.Text = "IF"
+        btnIF.UseVisualStyleBackColor = True
+        ' 
+        ' btnIF1
+        ' 
+        btnIF1.Location = New Point(12, 281)
+        btnIF1.Name = "btnIF1"
+        btnIF1.Size = New Size(94, 29)
+        btnIF1.TabIndex = 6
+        btnIF1.Text = "IF1"
+        btnIF1.UseVisualStyleBackColor = True
+        ' 
+        ' btnIF2
+        ' 
+        btnIF2.Location = New Point(12, 316)
+        btnIF2.Name = "btnIF2"
+        btnIF2.Size = New Size(94, 29)
+        btnIF2.TabIndex = 7
+        btnIF2.Text = "IF2"
+        btnIF2.UseVisualStyleBackColor = True
+        ' 
+        ' btnIF3
+        ' 
+        btnIF3.Location = New Point(12, 351)
+        btnIF3.Name = "btnIF3"
+        btnIF3.Size = New Size(94, 29)
+        btnIF3.TabIndex = 8
+        btnIF3.Text = "IF3"
+        btnIF3.UseVisualStyleBackColor = True
+        ' 
+        ' btnCase
+        ' 
+        btnCase.Location = New Point(169, 246)
+        btnCase.Name = "btnCase"
+        btnCase.Size = New Size(94, 29)
+        btnCase.TabIndex = 9
+        btnCase.Text = "CASE"
+        btnCase.UseVisualStyleBackColor = True
+        ' 
+        ' btnForNext
+        ' 
+        btnForNext.Location = New Point(371, 246)
+        btnForNext.Name = "btnForNext"
+        btnForNext.Size = New Size(94, 29)
+        btnForNext.TabIndex = 10
+        btnForNext.Text = "FOR NEXT"
+        btnForNext.UseVisualStyleBackColor = True
+        ' 
+        ' btnDoLoop
+        ' 
+        btnDoLoop.Location = New Point(371, 281)
+        btnDoLoop.Name = "btnDoLoop"
+        btnDoLoop.Size = New Size(94, 29)
+        btnDoLoop.TabIndex = 11
+        btnDoLoop.Text = "Do Loop"
+        btnDoLoop.UseVisualStyleBackColor = True
+        ' 
+        ' btnDoLoop1
+        ' 
+        btnDoLoop1.Location = New Point(371, 316)
+        btnDoLoop1.Name = "btnDoLoop1"
+        btnDoLoop1.Size = New Size(94, 29)
+        btnDoLoop1.TabIndex = 12
+        btnDoLoop1.Text = "Do Loop1"
+        btnDoLoop1.UseVisualStyleBackColor = True
+        ' 
+        ' btnDoLoop2
+        ' 
+        btnDoLoop2.Location = New Point(371, 351)
+        btnDoLoop2.Name = "btnDoLoop2"
+        btnDoLoop2.Size = New Size(94, 29)
+        btnDoLoop2.TabIndex = 13
+        btnDoLoop2.Text = "Do Loop2"
+        btnDoLoop2.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnDoLoop2)
+        Controls.Add(btnDoLoop1)
+        Controls.Add(btnDoLoop)
+        Controls.Add(btnForNext)
+        Controls.Add(btnCase)
+        Controls.Add(btnIF3)
+        Controls.Add(btnIF2)
+        Controls.Add(btnIF1)
+        Controls.Add(btnIF)
+        Controls.Add(btnOperator2)
+        Controls.Add(btnOperator1)
+        Controls.Add(btnOperator)
+        Controls.Add(btnVariabel1)
+        Controls.Add(btnVariabel)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btnVariabel As Button
+    Friend WithEvents btnVariabel1 As Button
+    Friend WithEvents btnOperator As Button
+    Friend WithEvents btnOperator1 As Button
+    Friend WithEvents btnOperator2 As Button
+    Friend WithEvents btnIF As Button
+    Friend WithEvents btnIF1 As Button
+    Friend WithEvents btnIF2 As Button
+    Friend WithEvents btnIF3 As Button
+    Friend WithEvents btnCase As Button
+    Friend WithEvents btnForNext As Button
+    Friend WithEvents btnDoLoop As Button
+    Friend WithEvents btnDoLoop1 As Button
+    Friend WithEvents btnDoLoop2 As Button
+
+End Class
---
+++ Peremuan1/Peremuan1/Form1.vb
@@ -0,0 +1,157 @@
+﻿Public Class Form1
+
+    ' Variabel
+    Public varPublic As String = "variabel Public dari Form1"
+    Private varDimForm1 As String = "variabel class Form1"
+
+    Private Sub btnVariabel_Click(sender As Object, e As EventArgs) Handles btnVariabel.Click
+        Dim lokalVar As Integer = 5 ' Variabel lokal
+        MessageBox.Show("Public : " & varPublic & vbCrLf &
+                "Lokal : " & lokalVar & vbCrLf &
+                "Class : " & varDimForm1)
+    End Sub
+
+    Private Sub btnVariabel1_Click(sender As Object, e As EventArgs) Handles btnVariabel1.Click
+        'lokalVar akan error karena berbeda prosedur, fungsi, modul
+        'MessageBox.Show("Public : " & varPublic & ", Lokal : " & lokalVar & ", Class : " & varDimForm1)
+        MessageBox.Show(String.Join(vbCrLf, "Public : " & varPublic, "Class : " & varDimForm1))
+    End Sub
+
+    'Tipe data dan Operator
+    Private Sub btnOperator_Click(sender As Object, e As EventArgs) Handles btnOperator.Click
+        Dim a As Integer = 17
+        Dim b As Integer = 2
+
+        Dim hasilPenjumlahan As Integer = a + b
+        Dim hasilPembagian As Double = a / b
+        Dim hasilPengurangan As Integer = a - b
+        Dim hasilPerkalian As Integer = a * b
+        Dim hasilMod As Integer = a Mod b
+        Dim hasilPangkat As Double = a ^ b
+
+        MessageBox.Show(String.Join(vbCrLf,
+                        a & " + " & b & " = " & hasilPenjumlahan,
+                        a & " / " & b & " = " & hasilPembagian,
+                        a & " - " & b & " = " & hasilPengurangan,
+                        a & " * " & b & " = " & hasilPerkalian,
+                        a & " Mod " & b & " = " & hasilMod,
+                        a & " ^ " & b & " = " & hasilPangkat))
+    End Sub
+
+    Private Sub btnOperator1_Click(sender As Object, e As EventArgs) Handles btnOperator1.Click
+        Dim x As Boolean = True
+        Dim y As Boolean = False
+
+        Dim hasilAnd As Boolean = x And y   ' False (karena salah satu False)
+        Dim hasilOr As Boolean = x Or y     ' True (karena salah satu True)
+        Dim hasilNotX As Boolean = Not x    ' False (karena Not membalik nilai)
+        Dim hasilXor As Boolean = x Xor y   ' True (karena salah satu True, tapi tidak keduanya)
+
+        MessageBox.Show(String.Join(vbCrLf,
+                        x & " And " & y & " = " & hasilAnd,
+                        x & " Or " & y & " = " & hasilOr,
+                        "Not " & x & " = " & hasilNotX,
+                        x & " Xor " & y & " = " & hasilXor))
+    End Sub
+
+    Private Sub btnOperator2_Click(sender As Object, e As EventArgs) Handles btnOperator2.Click
+        Dim a As Integer = 10
+        Dim b As Integer = 5
+
+        Dim hasilSama As Boolean = (a = b)    ' False
+        Dim hasilTidakSama As Boolean = (a <> b)  ' True
+        Dim hasilLebihBesar As Boolean = (a > b)  ' True
+        Dim hasilLebihKecil As Boolean = (a < b)  ' False
+        Dim hasilLebihBesarSama As Boolean = (a >= b) ' True
+        Dim hasilLebihKecilSama As Boolean = (a <= b) ' False
+
+        MessageBox.Show(String.Join(vbCrLf,
+                        a & " = " & b & " : " & hasilSama,
+                        a & " <> " & b & " : " & hasilTidakSama,
+                        a & " > " & b & " : " & hasilLebihBesar,
+                        a & " < " & b & " : " & hasilLebihKecil,
+                        a & " >= " & b & " : " & hasilLebihBesarSama,
+                        a & " <= " & b & " : " & hasilLebihKecilSama))
+    End Sub
+
+    ' Percabangan IF
+    Private Sub btnIF_Click(sender As Object, e As EventArgs) Handles btnIF.Click
+        Dim x As Integer = 11
+        If x > 10 Then
+            MessageBox.Show(x & " lebih besar dari 10")
+        End If
+    End Sub
+
+    Private d As Integer = 11
+    Private Sub btnIF1_Click(sender As Object, e As EventArgs) Handles btnIF1.Click
+        If d > 10 Then
+            MessageBox.Show(d & " lebih besar dari 10")
+            d = d + 1
+        End If
+    End Sub
+
+    Private Sub btnIF2_Click(sender As Object, e As EventArgs) Handles btnIF2.Click
+        Dim x As Integer = 11
+        Dim y As Integer = 18
+        If x > 10 And y < 20 Then
+            MessageBox.Show("Kondisi terpenuhi")
+        End If
+    End Sub
+
+    Private Sub btnIF3_Click(sender As Object, e As EventArgs) Handles btnIF3.Click
+        Dim x As Integer = 11
+        If x > 10 Then
+            MessageBox.Show("x lebih besar dari 10")
+        ElseIf x = 10 Then
+            MessageBox.Show("x sama dengan 10")
+        Else
+            MessageBox.Show("x kurang dari 10")
+        End If
+    End Sub
+
+    'Percabangan SELECT CASE
+    Private Sub btnCase_Click(sender As Object, e As EventArgs) Handles btnCase.Click
+        Dim nilai As Integer = 85
+        Select Case nilai
+            Case 90 To 100
+                MessageBox.Show("Grade A")
+            Case 80 To 89
+                MessageBox.Show("Grade B")
+            Case 70 To 79
+                MessageBox.Show("Grade C")
+            Case Else
+                MessageBox.Show("Grade D")
+        End Select
+    End Sub
+
+    ' Perulangan
+    Private Sub btnForNext_Click(sender As Object, e As EventArgs) Handles btnForNext.Click
+        For i As Integer = 1 To 5
+            MessageBox.Show("Iterasi ke-" & i)
+        Next
+    End Sub
+
+    Private Sub btnDoLoop_Click(sender As Object, e As EventArgs) Handles btnDoLoop.Click
+        Dim i As Integer = 6
+        Do
+            MessageBox.Show("Iterasi ke-" & i)
+            i = i + 1
+        Loop While i <= 5
+    End Sub
+
+    Private Sub btnDoLoop1_Click(sender As Object, e As EventArgs) Handles btnDoLoop1.Click
+        Dim i As Integer = 4
+        Do While i <= 5
+            MessageBox.Show("Iterasi ke-" & i)
+            i = i + 1
+        Loop
+    End Sub
+
+    Private Sub btnDoLoop2_Click(sender As Object, e As EventArgs) Handles btnDoLoop2.Click
+        Dim i As Integer = 1
+        Do Until i > 5
+            MessageBox.Show("Iterasi ke-" & i)
+            i = i + 1
+        Loop
+    End Sub
+End Class
